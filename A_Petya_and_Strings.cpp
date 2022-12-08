c8ef860410bb457f2082715632525ca74e2a2ff2
Arnob-sen <arnobsen16.2@gmail.com>
2022-12-08 19:57:50 +0600

    codeforce_112_A

---
+++ A_Petya_and_Strings.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main()
+{
+    char s1[101], s2[101];
+    cin >> s1 >> s2;
+    //  strcmp(strlwr(a), strlwr(b));
+    cout << strcmp(strlwr(s1), strlwr(s2)) << endl;
+}
