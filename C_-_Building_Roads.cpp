f1d751748328c2004bc3c71d8dd53ae540c4beec
Arnob-sen <arnobsen16.2@gmail.com>
2023-02-15 22:50:28 +0600

    cses_building roads

--- C_-_Building_Roads.cpp
+++ C_-_Building_Roads.cpp
@@ -1,4 +1,4 @@
-// /*/
+
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
@@ -42,53 +42,3 @@ int main()
     for (ll i = 0; i < ans.size() - 1; i++)
         cout << ans[i] << " " << ans[i + 1] << endl;
 }
-
-/*/
-#include <bits/stdc++.h>
-using namespace std;
-
-#define MAXN 202020
-
-int n, m;
-vector<vector<int>> adj(MAXN);
-vector<bool> visited(MAXN, false);
-vector<int> bridges;
-
-void DFS(int node)
-{
-    visited[node] = true;
-    for (auto child : adj[node])
-    {
-        if (visited[child] == false)
-        {
-            DFS(child);
-        }
-    }
-}
-
-int main()
-{
-    cin >> n >> m;
-    for (int i = 0; i < m; i++)
-    {
-        int node1, node2;
-        cin >> node1 >> node2;
-        adj[node1].push_back(node2);
-        adj[node2].push_back(node1);
-    }
-    for (int i = 1; i <= n; i++)
-    {
-        if (visited[i] == false)
-        {
-            bridges.push_back(i);
-            DFS(i);
-        }
-    }
-    cout << bridges.size() - 1 << endl;
-    for (int i = 0; i < bridges.size() - 1; i++)
-    {
-        cout << bridges[i] << " " << bridges[i + 1] << endl;
-    }
-    return 0;
-}
-/*/
