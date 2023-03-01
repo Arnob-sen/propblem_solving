137d80024760485813bc3441b44c95c43c89b0ea
Arnob-sen <arnobsen16.2@gmail.com>
2023-03-01 23:09:29 +0600

    246A_codeforce_Buggy Sorting

---
+++ A_Buggy_Sorting.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+const int MAXN = 100000005;
+vector<bool> p(MAXN, false);
+// vector<ll> dp;
+#define fastio                            \
+    {                                     \
+        ios_base::sync_with_stdio(false); \
+        cin.tie(NULL);                    \
+    }
+ll ans1 = 0, ans2 = 0;
+ll gcd(ll a, ll b)
+{
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
+}
+int main()
+{
+    fastio
+        ll n;
+    cin >> n;
+    if (n == 1 || n == 2)
+        cout << -1 << endl;
+    else
+    {
+        for (ll i = n; i >= 1; i--)
+            cout << i << " ";
+    }
+}
