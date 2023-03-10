e093b70a7e01fabdc5a4c600f7afdda06bfbbecb
Arnob-sen <arnobsen16.2@gmail.com>
2023-03-10 11:40:00 +0600

    cses_longest_flight_route

---
+++ F_Longest_Flight_Route.cpp
@@ -0,0 +1,115 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pb push_back
+#define F first
+#define S second
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define pii pair<int, int>
+#define vi vector<int>
+#define vii vector<pii>
+#define vc vector
+vector<ll> parent;
+#define IOS                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(0);                       \
+    cout.tie(0);
+
+const ll inf = 0;
+const ld ep = 0.0000001;
+const ld pi = acos(-1.0);
+const ll md = 1000000007;
+
+int vis[100005], dis[100005] = {0};
+vii adj[100005];
+
+ll n, m;
+void dijkstra()
+{
+
+    priority_queue<pii, vii, greater<pii>> pq;
+    pq.push({1, 1});
+    dis[1] = 1;
+    while (!pq.empty())
+    {
+        // if (vis[n])
+        //     return;
+        int u = pq.top().second;
+        pq.pop();
+        // if (vis[u])
+        // continue;
+        vis[u] = 1;
+        //  ans.push_back(u);
+
+        for (auto x : adj[u])
+        {
+            int v = x.first;
+            ll w = x.second;
+
+            if (dis[u] + w > dis[v])
+            {
+                dis[v] = dis[u] + w;
+                pq.push(make_pair(dis[v], v));
+                parent[v] = u;
+            }
+        }
+    }
+}
+void printpath(ll n)
+{
+    if (parent[n] == -1)
+    {
+        cout << n;
+        return;
+    }
+    printpath(parent[n]);
+    cout << " " << n;
+}
+void solve()
+{
+
+    cin >> n >> m;
+    parent.assign(n + 1, -1);
+
+    for (ll i = 1; i <= m; i++)
+    {
+        int u, v, w;
+        cin >> u >> v;
+        adj[u].pb({v, 1});
+        // adj[v].pb({u, 1});
+    }
+
+    dijkstra();
+    //   if (dis[n] >)
+    // cout << dis[n] << endl;
+    if (dis[n] == 0)
+        cout << "IMPOSSIBLE\n";
+    else
+    {
+        vector<ll> ans;
+        for (int i = n; i != -1; i = parent[i])
+            ans.push_back(i);
+        cout << ans.size() << endl;
+        reverse(ans.begin(), ans.end());
+        for (ll i = 0; i < ans.size(); i++)
+            cout << ans[i] << " ";
+    }
+    // for (int i = 0; i < parent.size(); i++)
+    //   cout << parent[i] << " ";
+    // cout << ans.size() << " ";
+}
+signed main()
+{
+    IOS;
+
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+        // cout<<'\n';
+    }
+}
